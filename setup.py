b6a913886bdfb3916543c46d7aebc91aedfbdf15
Liam Bindle <liambindle@gmail.com>
2021-05-22 17:50:45 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='sparselt',
+    version='0.1.0',
+    author="Liam Bindle",
+    author_email="liam.bindle@gmail.com",
+    description="A small library for regridding Earth system model data.",
+    url="https://github.com/LiamBindle/sparselt",
+    project_urls={
+        "Bug Tracker": "https://github.com/LiamBindle/sparselt/issues",
+    },
+    packages=['sparselt'],
+    install_requires=[
+        'numpy',
+        'netcdf4',
+        'xarray',
+    ],
+)
