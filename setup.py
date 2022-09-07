665e293622642bf64ba223722f552096dfd3b9ae
Liam Bindle <lrbindle@gmail.com>
2022-09-07 09:57:35 -0700

    Updated version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sparselt',
-    version='0.1.0',
+    version='0.1.2',
     author="Liam Bindle",
     author_email="liam.bindle@gmail.com",
     description="A small library for regridding Earth system model data.",
